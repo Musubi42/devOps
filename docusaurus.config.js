ee354c9d6241b0b5cd23eb6634c09c8b38bfdf27
Musubi42 <raphael.dev@protonmail.com>
2023-05-11 21:02:57 +0200

    Fix editing GitHub URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Musubi42/devOps/tree/musudoc/docs',
+            'https://github.com/Musubi42/devOps/tree/musudoc/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Musubi42/devOps/tree/musudoc/blog',
+            'https://github.com/Musubi42/devOps/tree/musudoc/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
