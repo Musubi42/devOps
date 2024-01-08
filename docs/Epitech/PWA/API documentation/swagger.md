c93316c6a81c6b34f7d047a7d0bf179e165404e6
Musubi42 <thibaut.raphael@protonmail.com>
2024-01-08 22:28:43 +0100

    fix(PWA) doc swagger iframe in a React component

--- docs/Epitech/PWA/API documentation/swagger.md
+++ docs/Epitech/PWA/API documentation/swagger.md
@@ -5,4 +5,4 @@ title: OpenAPI3.0 specfication
 
 # Open API3.0 specification
 
-<iframe class="full-screen-iframe" src="https://bump.sh/raphael/doc/pwa-cash-manager/operation/operation-register"></iframe>
+<Iframe iframeURL="https://bump.sh/raphael/doc/pwa-cash-manager/operation/operation-register" />
---
+++ src/components/docs/Iframe.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FRAME_STYLE = {
+  border: '1px solid #aaa',
+}
+
+/**
+ * Embeds a Storybook example
+ *
+ * @prop story The story ID in the URL to navigate to
+ */
+export default ({ iframeURL = '' }) => (
+  <iframe
+    title="Excalibur Storybook Example"
+    src={`${iframeURL}`}
+    frameBorder={0}
+    width="100%"
+    height="600"
+    style={FRAME_STYLE}
+  ></iframe>
+)
